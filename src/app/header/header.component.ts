5bc9611406cc74591ea6d1f21dd262b52d526fb1
Luis Paulo <luiss_paullo@hotmail.com>
2023-12-13 14:41:01 -0300

    about

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
+  
+  /**
+   * Role a página suavemente até a seção especificada pelo ID.
+   *
+   * @param sectionId O ID da seção para a qual rolar.
+   * @returns void
+   */
+  scrollToSection(sectionId: string): void {
+    const element = document.getElementById(sectionId);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
 
+  scrolled = false;
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    // Verifica a posição de rolagem
+    const scrollPosition = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+
+    // Define a condição para a mudança de cor
+    this.scrolled = scrollPosition > 50; // por exemplo, mude a cor após rolar 50 pixels
+  }
 }
