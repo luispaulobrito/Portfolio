df4bf121e3a104e4b4dcd3d85e265b8732003e2e
Luis Paulo <luiss_paullo@hotmail.com>
2023-12-21 14:25:18 -0300

    ajustes portifolio e footer

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -9,6 +9,14 @@ import { Component } from '@angular/core';
 export class PortfolioComponent {
   portfolios: Array<Portfolio> = [
     {
+      screen: '../../assets/images/explorador-orion-mobile.gif',
+      name: 'Explorador Orion',
+      description:
+        'Explorador Orion é uma aplicação responsiva em Angular que consome uma API em Node e visa fornecer acesso a dados da meteorologia de Marte. Foi desenvolvida em uma equipe de 7 pessoas (3 devs front, 2 back e 2 POs) utilizando SCRUM como metodologia ágil. A aplicação possui sistema de login para se obter acesso aos dados meteorológicos, uma newsletter para que o usuário cadastrado possa receber artigos diários relacionados ao assunto e uma comunidade no discord foi agregada ao projeto',
+      github: 'https://github.com/luispaulobrito/Orion-Bootcamp',
+      page: '',
+    },
+    {
       screen: '../../assets/images/projeto5-clientes-servicos.png',
       name: 'Projeto Clientes e Serviços Prestados',
       description:
@@ -16,24 +24,6 @@ export class PortfolioComponent {
       github:
         'https://github.com/luispaulobrito/Projeto-Clientes-e-Servicos-Prestados',
       page: '',
-    },
-    {
-      screen: '../../assets/images/projeto1-calculadora.png',
-      name: 'Calculadora',
-      description:
-        'Aplicação web e mobile construída para representar uma calculadora online. Nesse projeto foi aprendido: como estruturar um layout utilizando HTML, CSS e JavaScript.',
-      github:
-        'https://github.com/luispaulobrito/Projetos-Pessoais/tree/main/02%20-%20calculadora-sofisticada-js',
-      page: 'https://calculadoralp.netlify.app/',
-    },
-    {
-      screen: '../../assets/images/projeto2-imc.png',
-      name: 'Calculadora IMC',
-      description:
-        'Calculadora IMC é uma aplicação web e mobile construída para apresentar a avaliação do imc de uma pessoa de acordo com os dados fornecidos.',
-      github:
-        'https://github.com/luispaulobrito/Projetos-Pessoais/tree/main/04%20-%20calculadora-imc',
-      page: 'https://calculadoraimclp.netlify.app/',
     },
     {
       screen: '../../assets/images/projeto-agenda-de-contato.png',
@@ -45,6 +35,25 @@ export class PortfolioComponent {
       page: '',
     },
     {
+      screen: '../../assets/images/projeto2-imc.png',
+      name: 'Calculadora IMC',
+      description:
+        'Calculadora IMC é uma aplicação web e mobile construída para apresentar a avaliação do imc de uma pessoa de acordo com os dados fornecidos.',
+      github:
+        'https://github.com/luispaulobrito/Projetos-Pessoais/tree/main/04%20-%20calculadora-imc',
+      page: 'https://calculadoraimclp.netlify.app/',
+    },
+    {
+      screen: '../../assets/images/projeto1-calculadora.png',
+      name: 'Calculadora',
+      description:
+        'Aplicação web e mobile construída para representar uma calculadora online. Nesse projeto foi aprendido: como estruturar um layout utilizando HTML, CSS e JavaScript.',
+      github:
+        'https://github.com/luispaulobrito/Projetos-Pessoais/tree/main/02%20-%20calculadora-sofisticada-js',
+      page: 'https://calculadoralp.netlify.app/',
+    },
+
+    {
       screen: '../../assets/images/projeto3-formulario-com-validacao.png',
       name: 'Formulário com Validação',
       description:
@@ -54,13 +63,13 @@ export class PortfolioComponent {
       page: 'https://formchallengejs.netlify.app/',
     },
     {
-      screen: '../../assets/images/projeto4-single-price-component.png',
-      name: 'Single Price Grid Component',
+      screen: '../../assets/images/juros-compostos.jpg',
+      name: 'Calculadora de Juros Compostos',
       description:
-        'Single price grid component é uma aplicação web e mobile construída a partir do desafio no site Frontend Mentor. Nesse projeto foi aprendido: como estruturar um layout utilizando HTML e CSS. No CSS foi utilizado Grid e também foi aplicado responsividade.',
+        'Calculadora de Juros Compostos é uma aplicação responsiva construída utilizando Angular e Bootstrap com o objetivo de calcular os juros correspondentes aos valores dados pelo usuário com o passar de anos ou meses.',
       github:
-        'https://github.com/luispaulobrito/Frontend-Mentor/tree/main/01%20-%20single-price-grid-component-master',
-      page: 'https://gridcomponentsingleprice.netlify.app/',
+        'https://github.com/luispaulobrito/AngularLoiane/tree/main/exercicio-spa/calculadora-juros-compostos',
+      page: 'https://calculadora-juroscompostos.netlify.app/',
     },
     {
       screen: '../../assets/images/projeto-apigithub.gif',
